e51dd1dbbb785c94aff24fe9c157fd08fa69f189
Caleb Cushing <xenoterracide@gmail.com>
2024-06-24 15:02:18 -0400

    feat: add requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module org.mockito {
+    requires java.instrument;
+    requires jdk.attach;
+    requires net.bytebuddy;
+    requires net.bytebuddy.agent;
+    requires org.opentest4j;
+    requires static junit;
     exports org.mockito;
     exports org.mockito.codegen;
     exports org.mockito.configuration;
