fb175bbe2d8b1d2bd5042c3d852037ff7cd6b372
Caleb Cushing <xenoterracide@gmail.com>
2024-06-24 14:36:08 -0400

    feat: add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module org.mockito {
+    exports org.mockito;
+    exports org.mockito.codegen;
+    exports org.mockito.configuration;
+    exports org.mockito.creation.instance;
+    exports org.mockito.exceptions.base;
+    exports org.mockito.exceptions.misusing;
+    exports org.mockito.exceptions.verification;
+    exports org.mockito.exceptions.verification.junit;
+    exports org.mockito.exceptions.verification.opentest4j;
+    exports org.mockito.hamcrest;
+    exports org.mockito.invocation;
+    exports org.mockito.junit;
+    exports org.mockito.listeners;
+    exports org.mockito.mock;
+    exports org.mockito.plugins;
+    exports org.mockito.quality;
+    exports org.mockito.session;
+    exports org.mockito.stubbing;
+    exports org.mockito.verification;
+}
